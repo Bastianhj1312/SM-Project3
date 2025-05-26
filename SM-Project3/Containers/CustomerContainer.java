877d28b5439181481213d16dd64fbb50d9452a44
Thor-droid <10512525@ucn.dk>
2025-05-26 11:33:01 +0200

    Lavet customercontainer

--- SM-Project3/Containers/CustomerContainer.java
+++ SM-Project3/Containers/CustomerContainer.java
@@ -10,6 +10,35 @@ import java.util.ArrayList;
  */
 public class CustomerContainer
 {
-   private ArrayList<Customer> customer;
-   private Customer customer;
+   private ArrayList<Customer> customers;
+   private static CustomerContainer instance;
+   
+   private CustomerContainer(){
+       customers = new ArrayList<>();
+   }
+   public static CustomerContainer getInstance(){
+       if(instance == null){
+           instance = new CustomerContainer();
+       }
+       return instance;
+   }
+   public void saveCustomer(Customer customer){
+       customers.add(customer);
+   }
+   public Customer findCustomer(int phone){
+       Customer customer = null;
+       boolean found = false;
+       int i = 0;
+       while(!found && i < customers.size()){
+           Customer c = customers.get(i);
+           if(c.getPhone()==phone){
+               customer = c;
+               found = true;
+           }
+           else {
+               i++;
+           }
+       }
+       return customer;
+   }
 }
