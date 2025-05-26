6b579fa34badf3aec5fc1b276815ee6c355986ae
Patrick McQuillan <10667962@ucn.dk>
2025-05-26 13:06:12 +0200

    Order er blevet rykket rigtigt på plads

--- SM-Project3/Models/Order.java
+++ SM-Project3/Models/Order.java
@@ -1,16 +1,32 @@
 package Models;
-
+import java.util.ArrayList;
+import java.util.List;
 
 public class Order {
-private int staffID;
+    private int staffID;
+    private List<orderLine> orderLine;
+    private Customer customer;
 
-public Order(int staffID){
-    this.staffID = staffID;
-}
-public int getstaffID(){
-    return staffID;
-}
-public void setStaffID(int staffID){
-    this.staffID = staffID;
-}
+    // Constructor
+    public Order(Customer customer, int staffID) {
+        this.customer = customer;
+        this.staffID = staffID;
+        this.orderLine = new ArrayList<>();
+    }
+
+    // Getter for staffID
+    public int getStaffID() {
+        return staffID;
+    }
+
+    // Setter for staffID
+    public void setStaffID(int staffID) {
+        this.staffID = staffID;
+    }
+
+    // Method to add an order line
+    public void addOrderLine(Product p, int quantity) {
+        orderLine newOrderLine = new orderLine(p, quantity);
+        orderLine.add(newOrderLine);
+    }
 }
