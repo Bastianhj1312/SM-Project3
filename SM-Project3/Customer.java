4bb3b3cdfa69792f741c5d49e99203b2b7d13564
Thor-droid <10512525@ucn.dk>
2025-05-26 10:58:49 +0200

    Laver de forskellige simple klasser

--- SM-Project3/Customer.java
+++ SM-Project3/Customer.java
@@ -1,4 +1,21 @@
 
 public class Customer {
-
+private String name;
+private int phone;
+public Customer(String name, int phone){
+    this.name = name;
+    this.phone = phone;
+}
+public String getName(){
+    return name;
+}
+public void setName(String name){
+    this.name = name;
+}
+public int getPhone(){
+    return phone;
+}
+public void setPhone(int phone){
+    this.phone = phone;
+}
 }
--- SM-Project3/Order.java
+++ SM-Project3/Order.java
@@ -1,4 +1,14 @@
 
 public class Order {
+private int staffID;
 
+public Order(int staffID){
+    this.staffID = staffID;
+}
+public int getstaffID(){
+    return staffID;
+}
+public void setStaffID(int staffID){
+    this.staffID = staffID;
+}
 }
