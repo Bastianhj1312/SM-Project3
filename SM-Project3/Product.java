8b18761837fd3dfc1e6f17ae4c08323966a119da
Bastianhj1312 <10665995@ucn.dk>
2025-05-26 11:01:58 +0200

    product lavet

--- SM-Project3/Product.java
+++ SM-Project3/Product.java
@@ -1,4 +1,26 @@
-
 public class Product {
+    private int productNo;
+    private int quantity;
+    
+    public Product(int productNo, int quantity){
+        this.productNo = productNo;
+        this.quantity = quantity;
+    }
+    
+    public int getProductNo(){
+        return productNo;
+    }
+    
+    public void setProductNo(int productNo){
+        this.productNo = productNo;
+    }
+    
+    public int getQuantity(){
+        return quantity;
+    }
+    
+    public void setQuantity(int quantity){
+        this.quantity = quantity;
+    }
 
 }
