7c10e39aebbec3123406e06acc1579e0d8b96ff7
Thor-droid <10512525@ucn.dk>
2025-05-26 14:36:04 +0200

    test

--- SM-Project3/Containers/ProductContainer.java
+++ SM-Project3/Containers/ProductContainer.java
@@ -45,11 +45,30 @@ public class ProductContainer
         return product;
     }
      public void printAllProducts() {
-        for (Product p : products) {
-            System.out.println("Product No: " + p.getProductNo());
-            System.out.println("Quantity: " + p.getQuantity());
-            System.out.println("Price: " + p.getPrice());
-            System.out.println("-----");
+    for (Product p : products) {
+        System.out.println("Product No: " + p.getProductNo());
+        System.out.println("Quantity: " + p.getQuantity());
+        System.out.println("Description: " + p.getDescription());
+        System.out.println("Category: " + p.getCategory());
+        System.out.println("Selling Price: " + p.getSellingPrice());
+        System.out.println("Cost Price: " + p.getCostPrice());
+        System.out.println("Total Price: " + p.getPrice());
+
+        if (p instanceof SimpleProduct) {
+            SimpleProduct sp = (SimpleProduct) p;
+            System.out.println("Product Name: " + sp.getProductName());
+            System.out.println("Manufacturer: " + sp.getManufacturer());
+            System.out.println("Model: " + sp.getModel());
+            System.out.println("Recommended Retail Price: " + sp.getRecommendedRetailPrice());
+            System.out.println("Supplier ID: " + sp.getSupplierId());
+            System.out.println("SKU: " + sp.getSku());
+        } else if (p instanceof UniqueProduct) {
+            UniqueProduct up = (UniqueProduct) p;
+            System.out.println("Serial Number: " + up.getSerialNumber());
         }
+
+        System.out.println("-----");
     }
+}
+
 }
--- SM-Project3/UI/Main.java
+++ SM-Project3/UI/Main.java
@@ -7,12 +7,21 @@ public class Main {
     public static void main(String[] args) {
         ProductContainer container = ProductContainer.getInstance();
 
-        Product p1 = new SimpleProduct(101, 5, 20.0);
-        Product p2 = new UniqueProduct(202, 499.0);
+        // Create a simple product
+        SimpleProduct p1 = new SimpleProduct(
+            101, 5, "USB Cable", "Logitech", "X200", 20.0, "SUP001", "SKU101"
+        );
 
+        // Create a unique product
+        UniqueProduct p2 = new UniqueProduct(
+            202, "SN-0001", 499.0
+        );
+
+        // Save products in container
         container.saveProduct(p1);
         container.saveProduct(p2);
 
+        // Print all products
         container.printAllProducts();
     }
 }
