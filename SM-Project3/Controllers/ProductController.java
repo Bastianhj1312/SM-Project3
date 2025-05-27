3dcfe913609e46d44429d7a9828cb49132314fa1
Bastianhj1312 <10665995@ucn.dk>
2025-05-27 10:17:39 +0200

    ProductController

--- SM-Project3/Controllers/ProductController.java
+++ SM-Project3/Controllers/ProductController.java
@@ -1,5 +1,6 @@
 package Controllers;
-
+import Models.*;
+import Containers.*;
 
 /**
  * Write a description of class ProductController here.
@@ -7,29 +8,16 @@ package Controllers;
  * @author (your name)
  * @version (a version number or a date)
  */
-public class ProductController
-{
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class ProductController
-     */
-    public ProductController()
-    {
-        // initialise instance variables
-        x = 0;
+public class ProductController {
+    ProductContainer productcontainer;
+    Product product;
+    
+    public ProductController(){
+        productcontainer = ProductContainer.getInstance();
     }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+    
+    public Product findProduct(int productNo){
+       return productcontainer.findProduct(productNo);
     }
+    
 }
