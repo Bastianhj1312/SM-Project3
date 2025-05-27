bf4b420bd5ef96dcc54fe8bba81dc135a7773008
Bastianhj1312 <10665995@ucn.dk>
2025-05-27 13:58:07 +0200

    CustomerUI done

--- SM-Project3/UI/CustomerUI.java
+++ SM-Project3/UI/CustomerUI.java
@@ -3,7 +3,6 @@ import Models.*;
 import Containers.*;
 import Controllers.*;
 import java.util.Scanner;
-//import TextOptions;
 
 
 /**
@@ -15,6 +14,7 @@ import java.util.Scanner;
 public class CustomerUI
 {
     private CustomerController customercontroller;
+    private CustomerContainer customercontainer;
     
     public CustomerUI(){
         customercontroller = new CustomerController();
@@ -27,7 +27,7 @@ public class CustomerUI
     private void customerMenu() {
        boolean exit = false;
        while(!exit) {
-          int choice = writeCustomerMenu(); 
+           int choice = writeOrderMenu();
           if(choice == 1) { //create the customer
             createCustomer();
           }
@@ -41,26 +41,22 @@ public class CustomerUI
        }//end while
     }
     
-    private String inputCustomerName() {
-         // makes an object keyboard to have input from the screen
-         Scanner keyboard = new Scanner(System.in);  
-         System.out.println(" Indtast navn på kunden:  ");
-         String name = keyboard.nextLine();
-         return name;
-    }
-    
-    private String inputCustomerPhone() {
-        // makes an object keyboard to read input from the console
-        Scanner keyboard = new Scanner(System.in);
-        System.out.println(" Indtast kundens telefonnummer: ");
-        String phone = keyboard.nextLine();
-        return phone;
+     private int writeOrderMenu() {
+        // creates a keyboard object to read input
+        TextOptions menu = new TextOptions("\n ***** Kunde menu *****", "Tilbage"); 
+        menu.addOption("Opret en profil");
+
+        
+        int choice = menu.prompt();
+
+        return choice;
     }
     
     private void createCustomer() {
-        String name = TextInput.inputString();
-        String phone = inputCustomerPhone();
-        customercontroller.saveCustomer(name, phone);
+        String name = TextInput.inputString("Tilføj kundens navn: ");
+        int phone = TextInput.inputNumber("Tilføj kundens telefon nummer: ");
+        Customer customer = new Customer(name, phone);
+        customercontainer.saveCustomer(customer);
     }  
 
 }
