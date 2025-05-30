68c23dc4d9b486bc7bd4e93e164567632f90e653
Bastianhj1312 <10665995@ucn.dk>
2025-05-30 10:04:31 +0200

    Create UniqueProductCopy.java

---
+++ SM-Project3/Models/UniqueProductCopy.java
@@ -0,0 +1,13 @@
+package Models;
+
+
+/**
+ * Write a description of class UniqueProductCopy here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class UniqueProductCopy
+{
+   
+}
